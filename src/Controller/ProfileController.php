4a1993691f498512b01379254fe805026350d3f5
rejhad2410 <rejhad2000@gmail.com>
2026-03-09 10:37:55 +0100

    edit profile done

---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ProfileFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[IsGranted('ROLE_USER')]
+class ProfileController extends AbstractController
+{
+    #[Route('/profile/edit', name: 'app_profile_edit')]
+    public function edit(Request $request, EntityManagerInterface $em, UserPasswordHasherInterface $hasher): Response
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(ProfileFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Si un nouveau mot de passe a été saisi, on le hash et on l'enregistre
+            $newPassword = $form->get('newPassword')->getData();
+            if ($newPassword) {
+                $user->setPassword($hasher->hashPassword($user, $newPassword));
+            }
+
+            $em->flush();
+            $this->addFlash('success', 'Profil mis à jour avec succès.');
+
+            return $this->redirectToRoute('app_profile_edit');
+        }
+
+        return $this->render('profile/edit.html.twig', [
+            'form' => $form,
+        ]);
+    }
+}
