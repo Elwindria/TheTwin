cc699b65fa507ad682d33bd0ccec9c0dc6e3ba7e
Boushow <robert.bad.etu@gmail.com>
2026-03-09 03:12:26 -0300

    feat: landing page avec le footer et le logo

--- assets/app.js
+++ assets/app.js
@@ -8,6 +8,7 @@ import './stimulus_bootstrap.js';
  */
 import './styles/app.css';
 import './styles/auth.css';
+import './styles/landing.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
 
