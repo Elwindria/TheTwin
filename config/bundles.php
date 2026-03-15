e041d98da07ee378c005df5d757c4641a73aac61
Elwindria <p.lopez2000@laposte.net>
2026-03-15 17:33:52 +0100

    Enable fixtures bundle on staging deploy

--- config/bundles.php
+++ config/bundles.php
@@ -15,5 +15,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true, 'test' => true],
     Symfony\UX\React\ReactBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['all' => true, 'test' => true],
 ];
