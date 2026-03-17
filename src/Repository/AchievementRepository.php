963cf45fd87dca40add1d2005419643e33ed17a2
Elwindria <p.lopez2000@laposte.net>
2026-03-17 22:42:46 +0100

    Rajout d'une méthode pour récupérer tous les achievement par utilisateur

--- src/Repository/AchievementRepository.php
+++ src/Repository/AchievementRepository.php
@@ -15,29 +15,4 @@ class AchievementRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Achievement::class);
     }
-
-    //    /**
-    //     * @return Achievement[] Returns an array of Achievement objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('a')
-    //            ->andWhere('a.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('a.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?Achievement
-    //    {
-    //        return $this->createQueryBuilder('a')
-    //            ->andWhere('a.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
 }
--- src/Repository/UserAchievementRepository.php
+++ src/Repository/UserAchievementRepository.php
@@ -16,28 +16,15 @@ class UserAchievementRepository extends ServiceEntityRepository
         parent::__construct($registry, UserAchievement::class);
     }
 
-    //    /**
-    //     * @return UserAchievement[] Returns an array of UserAchievement objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('u')
-    //            ->andWhere('u.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('u.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?UserAchievement
-    //    {
-    //        return $this->createQueryBuilder('u')
-    //            ->andWhere('u.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+    public function findAllWithUserAndAchievement(): array
+    {
+        return $this->createQueryBuilder('ua')
+            ->leftJoin('ua.user', 'u')
+            ->addSelect('u')
+            ->leftJoin('ua.achievement', 'a')
+            ->addSelect('a')
+            ->orderBy('u.id', 'ASC')
+            ->getQuery()
+            ->getResult();
+    }
 }
