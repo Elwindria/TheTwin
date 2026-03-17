149183d5ff9da1f59380747745a81917f7516d59
Elwindria <p.lopez2000@laposte.net>
2026-03-18 00:45:35 +0100

    Rajout d'une methode pour attribuer les achivement en fonction du summary pour chauqe user

--- src/Service/AchievementAwarderService.php
+++ src/Service/AchievementAwarderService.php
@@ -2,12 +2,20 @@
 
 namespace App\Service;
 
+use App\Repository\UserRepository;
+use App\Repository\AchievementRepository;
 use App\Service\EcoMetricsService;
+use App\Entity\UserAchievement;
+use Doctrine\ORM\EntityManagerInterface;
+
 
 class AchievementAwarderService
 {
     public function __construct(
         private EcoMetricsService $ecoMetricsService,
+        private UserRepository $userRepository,
+        private AchievementRepository $achievementRepository,
+        private EntityManagerInterface $entityManager,
     ) 
     {
 
@@ -15,23 +23,43 @@ class AchievementAwarderService
 
     public function awardAchievementByUsers(): void
     {
-            $dataByUsers = $this->ecoMetricsService->getSummaryByAllUsers();
+        $dataByUsers = $this->ecoMetricsService->getSummaryByAllUsers();
 
-            foreach ($dataByUsers as $userData) {
-                $userId = $userData['user_id'];
-                $summary = $userData['summary'];
-                $ownedCodes = $userData['owned_achievement_codes'];
-                $rules = $userData['rules'];
+        foreach ($dataByUsers as $userData) {
+            $userId = $userData['user_id'];
+            $summary = $userData['summary'];
+            $ownedCodes = $userData['owned_achievement_codes'];
+            $rules = $userData['rules'];
 
-                foreach ($rules as $rule) {
-                    $type = $rule['type'];
-                    $code = $rule['code'];
-                    $threshold = $rule['threshold'];
+            foreach ($rules as $rule) {
+                $type = $rule['type'];
+                $code = $rule['code'];
+                $threshold = $rule['threshold'];
 
-                    if ($summary[$type] >= $threshold) {
-                        
-                    }
+                if (($summary[$type] ?? 0) >= $threshold
+                    && !in_array($code, $ownedCodes, true))
+                {
+                    $user = $this->userRepository->find($userId);
+                    $achievement = $this->achievementRepository->findOneBy([
+                        'code' => $code,
+                    ]);
+
+                    $userAchievement = new UserAchievement();
+                    $userAchievement->setUser($user);
+                    $userAchievement->setAchievement($achievement);
+                    $userAchievement->setAwardedAt(new \DateTimeImmutable());
+
+                    $this->entityManager->persist($userAchievement);
+
+                    //eviter les doublons d'attribution
+                    $ownedCodes[] = $code;
                 }
             }
+        }
+
+        $this->entityManager->flush();
+
+        $patata = $this->achievementRepository->findAll();
+        dd($patata);
     }
 }
