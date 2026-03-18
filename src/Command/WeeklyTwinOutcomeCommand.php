4a2b8b2c91b359cd937007b1e9c29a824e40bd5e
Elwindria <p.lopez2000@laposte.net>
2026-03-18 22:25:11 +0100

    préparation de la commande pour lancer mon service avec Cron automatiquement

--- src/Command/WeeklyTwinOutcomeCommand.php
+++ src/Command/WeeklyTwinOutcomeCommand.php
@@ -7,15 +7,17 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use App\Service\WeeklyResolutionService;
 
 #[AsCommand(
     name: 'app:weekly-twin-outcome',
-    description: 'Attribue automatiquement les achievements aux utilisateurs',
+    description: 'Résout la semaine écoulée et prépare la suivante + Attribue automatiquement les achievements aux utilisateurs',
 )]
 class WeeklyTwinOutcomeCommand extends Command
 {
-    public function __construct()
-    {
+    public function __construct(
+        private WeeklyResolutionService $weeklyResolutionService,
+    ) {
         parent::__construct();
     }
 
@@ -23,7 +25,9 @@ class WeeklyTwinOutcomeCommand extends Command
     {
         $io = new SymfonyStyle($input, $output);
 
-        $io->success('Commande prête.');
+        $this->weeklyResolutionService->resolveCurrentWeek();
+
+        $io->success('La semaine a bien été résolue.');
 
         return Command::SUCCESS;
     }
